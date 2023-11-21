d501fc435d5b612a06ff80cddd9ea4257bc4160c
bagusdaffaf01@gmail.com <bagusdaffaf01@gmail.com>
2023-11-22 00:42:03 +0700

    update

--- laravel9/resources/views/dashboard.blade.php
+++ laravel9/resources/views/dashboard.blade.php
@@ -2,108 +2,161 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
 
-  <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
-  <title>Admin Dashboard</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
+  {{-- Bootstrap --}}
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+  {{-- CSS --}}
   <link rel="stylesheet" href="/css/dashboard.css">
+
 </head>
 <body>
-
-  <div class="container-fluid">
-    <div class="row">
-      <!-- Sidebar -->
-      <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block sidebar">
-        <div class="text-center">
-            <img src="https://via.placeholder.com/50" alt="Logo" class="rounded-circle mb-3">
-            <h4>Get Aplikasi</h4>
-        </div>
-          
-        <div class="position-sticky">
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">
-                <i data-feather="home"></i>  Dashboard
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <i data-feather="users"></i>  Peserta Magang
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <i data-feather="book"></i> Absensi
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <i data-feather="user"></i> Users
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <i data-feather="log-out"></i> Log Out
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
-      <!-- Content -->
-      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <div class="content">
-          <h1 class="mb-4">Dashboard</h1>
-          <div class="row">
-            <div class="col-lg-4 mb-4">
-              <!-- Card 1 -->
-              <div class="card">
-                <div class="card-header">
-                  Total Orders
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title">125</h5>
-                  <p class="card-text">New orders this month</p>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-4 mb-4">
-              <!-- Card 2 -->
-              <div class="card">
-                <div class="card-header">
-                  Total Products
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title">85</h5>
-                  <p class="card-text">Available products</p>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-4 mb-4">
-              <!-- Card 3 -->
-              <div class="card">
-                <div class="card-header">
-                  Total Customers
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title">50</h5>
-                  <p class="card-text">Active customers</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
+  <div class="sidebar">
+    <div class="logo"></div>
+    <ul class="menu">
+      <li class="active">
+        <a href="#">
+          <i class="fas fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fas fa-user"></i>
+          <span>Profile</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fas fa-chart-bar"></i>
+          <span>Statistics</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fas fa-briefcase"></i>
+          <span>Careers</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fas fa-question-circle"></i>
+          <span>FAQ</span>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fas fa-cog"></i>
+          <span>Setting</span>
+        </a>
+      </li>
+      <li class="logout">
+        <a href="#">
+          <i class="fas fa-sign-out-alt"></i>
+          <span>Logout</span>
+        </a>
+      </li>
+    </ul>
   </div>
 
-  <!-- Bootstrap JS, Popper.js, and jQuery (required for Bootstrap JavaScript plugins) -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  {{-- Content --}}
+  <div class="main--content">
+    <div class="header--wrapper">
+      <div class="header--title">
+        <span>Primary</span>
+        <h2>Dashboard</h2>
+      </div>
+      <div class="user--info">
+        <div class="search--box">
+          <i class="fa-solid fa-search"></i>
+          <input type="text" placeholder="Search">
+        </div>
+        <img src="/img/Meow.jpg" alt="">
+      </div>
+    </div>
 
-  <script>
-    feather.replace();
-  </script>
+    {{-- Card Content --}}
+
+    <div class="card--container">
+      <h3 class="main--title">Today data</h3>
+      <div class="card--wrapper">
+        <div class="payment--card">
+          <div class="card--header">
+            <div class="amount">
+              <span class="title">User</span>
+              <span class="amount--value">30 Orang</span>
+            </div>
+            <i class="fas fa-users icon"></i>
+          </div>
+          <span class="card--detail">Peserta Magang</span>
+        </div>
+
+        <div class="payment--card">
+          <div class="card--header">
+            <div class="amount">
+              <span class="title">Admin</span>
+              <span class="amount--value">2 Orang</span>
+            </div>
+            <i class="fas fa-user icon"></i>
+          </div>
+          <span class="card--detail">Administrator</span>
+        </div>
+      </div>
+    </div>
+
+    {{-- Table Content --}}
+    <div class="tabular--wrapper">
+      <h3 class="main--title">Data Peserta magang</h3>
+      <div class="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Asal Sekolah</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Bagus Daffa Firmansyah</td>
+                <td>Politeknik Negeri Jember</td>
+                <td><button>Tepat Waktu</button></td>
+              </tr>
+
+              <tr>
+                <td>1</td>
+                <td>Bagus Daffa Firmansyah</td>
+                <td>Politeknik Negeri Jember</td>
+                <td><button>Tepat Waktu</button></td>
+              </tr>
+
+              <tr>
+                <td>1</td>
+                <td>Bagus Daffa Firmansyah</td>
+                <td>Politeknik Negeri Jember</td>
+                <td><button>Tepat Waktu</button></td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colspan="7">Total: 50 Orang</td>
+              </tr>
+            </tfoot>
+        </table>
+      </div>
+    </div>
+
+  </div>
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>

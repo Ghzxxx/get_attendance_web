21153b0665d13b49179b498cc8200195335278c5
bagusdaffaf01@gmail.com <bagusdaffaf01@gmail.com>
2023-12-18 11:38:02 +0700

    ...

--- laravel9/routes/web.php
+++ laravel9/routes/web.php
@@ -23,10 +23,6 @@ Route::get('/', function () {
     return view('dashboard');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
-
 Route::get('/peserta', [DataPeserta::class, 'index']);
 Route::get('/peserta/{id}', [DataPeserta::class, 'show']);
 Route::get('/peserta-add', [DataPeserta::class, 'create']);
